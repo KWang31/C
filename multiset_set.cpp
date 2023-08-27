e865d6badee91afb1d9028cc12547f2aa48a715a
Kai Wang <35879380+KWang31@users.noreply.github.com>
2023-08-26 23:27:22 -0400

    Update and rename set.cpp to multiset_set.cpp

--- multiset_set.cpp
+++ multiset_set.cpp
@@ -3,11 +3,18 @@ using namespace std;
 int main() {
 
     set<int> s; // sorted set
+    multiset<int> ms; //sorted MULTIset
    vector<int> v = {3,5,7,2,3,1,9,8,7,3};
    for(int i: v){
-      s.insert(i);
+      s.insert(i), ms.insert(i);
    }
+
+    //Set gets rid of duplicates, multiset doesn't
+  cout<<"Sets"<<"\n";
    for(int i:s){
       cout<< i <<"\n";
    }
+   for(int i:ms){
+		cout << i << "\n";
+      }
 }
